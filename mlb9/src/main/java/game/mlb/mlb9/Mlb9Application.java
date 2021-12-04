49039a69a038d963471ca8462ffc5b9f2964cec1
Kevin Blaschke <kevinblaschke01@gmail.com>
2021-12-03 19:50:35 -0600

    init

---
+++ mlb9/src/main/java/game/mlb/mlb9/Mlb9Application.java
@@ -0,0 +1,13 @@
+package game.mlb.mlb9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mlb9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mlb9Application.class, args);
+	}
+
+}
---
+++ mlb9/src/test/java/game/mlb/mlb9/Mlb9ApplicationTests.java
@@ -0,0 +1,13 @@
+package game.mlb.mlb9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mlb9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
